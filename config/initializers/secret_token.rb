e3cf7743776d043436d25abefa56f94396ceb1c2
Thomas Chen <foxnewsnetwork@gmail.com>
2011-10-11 21:21:34 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MLDS::Application.config.secret_token = '1ad90633a73d666cc5582a03bd4c87dffdcd8f166bcf3c23e08d6afa1d4efe2b44faec42814b3b046ecfce22e643d9806b728759d9071797ce0b5fac2668b054'
