fc3537ccfef0628d27c39cda2e5ee8e7b7f49ebf
Thomas Chen <foxnewsnetwork@gmail.com>
2011-10-18 15:33:07 -0700

    finally fixed the text viewing and summary page; we now bring up a modal dialog box when the user clicks on the icons. More updates coming soon, but the priority of this has gone down since the site is largely working on. Next update would have to be a major back-end overhaul as I put in some more pages and models

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -19,16 +19,38 @@ class PagesController < ApplicationController
     
     @projects = Array.new
     @employees = Array.new
-    for i in 0..4
+    @projects.push({
+			:type => "projects",
+      :img => "#{@album}projects.png" ,
+      :url => "#{@album}projects.png" ,
+      :text => "CINEMA CubeSAT was a satellite aerospace project with the Space Science Labs at UC Berkeley. The sat is scheduled to launch before the end of 2011. I worked on the sun sensors on the satellite."
+    })
+    @projects.push({
+			:type => "projects",
+      :img => "#{@album}projects.png" ,
+      :url => "#{@album}projects.png" ,
+      :text => "Knowz Doze was a hardware arduino platform designed to be a blutooth attachment to help sleepy drivers stay awake during long drives. I worked on this project with my UC Berkeley team members."
+    })
+    for i in 2..4
       @projects.push({ 
+				:type => "projects",
         :img => "#{@album}Untitled-2.jpg" ,
         :url => "#{@album}project00#{i}.png" ,
-        :text => "stuff here"
+        :text => "Additional contents coming..."
       })
+    end
+    @employees.push({
+			:type => "employees",
+      :img => "#{@album}employee.png" ,
+      :url => "#{@album}employee.png" ,
+      :text => "Thomas Han Chen is the founder of ML-DS; a graduate student in ME by major he has since then opted to learn, do, and try a bit of everything in an effor to shape the future."
+    })
+    for i in 1..4
       @employees.push({ 
+				:type => "employees",
         :img => "#{@album}MahouShoujoonCrack.jpg" ,
         :url => "#{@album}employee00#{i}.png" ,
-        :text => "samefag text"
+        :text => "No one else here yet..."
       })
     end
   end
